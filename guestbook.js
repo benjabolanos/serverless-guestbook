59847963502b80d101c99fc4fdd422e2009fcbb5
benjabolanos <82410879+benjabolanos@users.noreply.github.com>
2022-07-22 01:49:29 -0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://44661dae.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
